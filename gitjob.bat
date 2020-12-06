f9e81ed718607b03ce4173171d96bf3b66b8552e
ronitrajat6297 <36653712+ronitrajat6297@users.noreply.github.com>
2020-12-06 14:19:54 -0500

    added branch

---
+++ gitjob.bat
@@ -0,0 +1,9 @@
+@echo off
+    cls
+    echo this is the test.bat batch file
+    pause
+    cls
+    echo hey! look at this! >>test.bat
+    cls
+    echo this is the last line of the test.bat... or is it?
+    Pause
